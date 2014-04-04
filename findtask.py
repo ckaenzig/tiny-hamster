fb989f15b6af42bc04fec4f4f0ff9732c22833dc
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-04-04 14:38:18 +0200

    upd findtask.py: add missing base url

--- findtask.py
+++ findtask.py
@@ -12,7 +12,7 @@ except IndexError:
   sys.exit(1)
 
 
-tiny = tinylib.TinyServer(tinyconf.user_name, tinyconf.user_pwd, tinyconf.tiny_db)
+tiny = tinylib.TinyServer(tinyconf.user_name, tinyconf.user_pwd, tinyconf.tiny_db, tinyconf.rpc_url)
 
 print
 for proj in tiny.search_account(proj_arg):
